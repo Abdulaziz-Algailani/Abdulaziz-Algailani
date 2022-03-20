25c34c92f4693786c2ae625c438b4c5af9af9eaf
Abdulaziz-Algailani <92076791+Abdulaziz-Algailani@users.noreply.github.com>
2022-03-20 22:49:15 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function printname(){
+    
+    document.getElementById("name").innerHTML="Abdulaziz Saleh Aljailani";
+
+}
+var x=2;
+if (x==2){
+    printname();
+}else{
+    document.getElementById("name").innerHTML="X is not equale 2";
+}
+
+
+
+
+
